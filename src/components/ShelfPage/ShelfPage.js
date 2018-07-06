531b903850726e024db1c66b0e51bad6e9a825a6
JacobTovsen <jacobtovsen@gmail.com>
2018-07-06 09:22:09 -0500

    shelfReducer STORE_ITEMS

---
+++ src/components/ShelfPage/ShelfPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Nav from '../../components/Nav/Nav';
+import { USER_ACTIONS } from '../../redux/actions/userActions';
+
+const mapStateToProps = state => ({
+    user: state.user,
+  });
+
+  export default connect(mapStateToProps)(InfoPage);
--- src/redux/reducers/shelfReducer.js
+++ src/redux/reducers/shelfReducer.js
@@ -1,11 +1,16 @@
 import { combineReducers } from 'redux';
 
 const shelfReducer = (state = [], action) => {
-
-}
+    switch (action.type) {
+        case 'STORE_ITEMS':
+            return action.payload;
+        default:
+            return state;
+    }
+};
 
 const countReducer = (state = [], action) => {
-
+    
 }
 
 export default combineReducers({
