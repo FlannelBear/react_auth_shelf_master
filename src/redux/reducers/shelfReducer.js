6c69088076f1d30159314a38b2a39c2fa17a2c58
Flannel Bear <josh.flannelbear@gmail.com>
2018-07-06 09:21:07 -0500

    completed countReducer

--- src/redux/reducers/shelfReducer.js
+++ src/redux/reducers/shelfReducer.js
@@ -5,7 +5,10 @@ const shelfReducer = (state = [], action) => {
 }
 
 const countReducer = (state = [], action) => {
-
+    if(action.type === 'STORE_COUNT'){
+        return action.payload
+    }
+    return state;
 }
 
 export default combineReducers({
