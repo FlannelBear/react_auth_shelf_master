a4bf4adea4479dc241ed5f9efa364cdc0c6b1ca9
Flannel Bear <josh.flannelbear@gmail.com>
2018-07-06 09:14:34 -0500

    set up reducers

---
+++ src/redux/reducers/shelfReducer.js
@@ -0,0 +1,14 @@
+import { combineReducers } from 'redux';
+
+const shelfReducer = (state = [], action) => {
+
+}
+
+const countReducer = (state = [], action) => {
+
+}
+
+export default combineReducers({
+    shelfReducer, 
+    countReducer
+});
--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -8,6 +8,7 @@ export default function* rootSaga() {
   yield all([
     userSaga(),
     loginSaga(),
+    shelfSaga(),
     // watchIncrementAsync()
   ]);
 }
