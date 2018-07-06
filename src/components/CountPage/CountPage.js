7d06ddef1a2cc419893774679db305abbeca86f1
Flannel Bear <josh.flannelbear@gmail.com>
2018-07-06 09:52:25 -0500

    add nav and auth

--- src/components/CountPage/CountPage.js
+++ src/components/CountPage/CountPage.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 
-const mapReduxToProps = (reduxStore) => ({
-    reduxStore
+import Nav from '../../components/Nav/Nav';
+import { USER_ACTIONS } from '../../redux/actions/userActions';
+
+const mapReduxToProps = state => ({
+    user: state.user,
+    count: state.shelf.countReducer
 });
 
 class CountPage extends Component{
 
     componentDidMount(){
+        this.props.dispatch({type: USER_ACTIONS.FETCH_USER});
         this.props.dispatch({type: 'FETCH_COUNT'});
     }
 
@@ -15,13 +20,16 @@ class CountPage extends Component{
 
         const countList = (
             <ul>
-            {this.props.reduxStore.shelf.countReducer.map(item => <li key={item.id}>User: {item.username} Posts: {item.total}</li>)}
+            {this.props.count.map(item => <li key={item.id}>User: {item.username} Posts: {item.total}</li>)}
             </ul>
         );
 
         return(
             <div>
+                <Nav/>
+                <div>
                 {countList}
+                </div>
             </div>
         );
     }
